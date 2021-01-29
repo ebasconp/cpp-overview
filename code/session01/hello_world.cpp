837dfa24fdfdd969bec3992944c5362256afe3c9
Ernesto Bascón Pantoja <ernesto.bascon.pantoja@gmail.com>
2021-01-28 23:15:24 -0400

    Session 01

---
+++ code/session01/hello_world.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#define PI 3.14
+
+// Directiva de preprocesador
+
+// Este es un comentario de línea
+
+/*
+Este no es un comentario
+  de una sola línea sino
+  * 
+  * 
+  * 
+  * 
+  * 
+  de varias
+*/
+
+int main()
+{
+	''
+	std::cout << "Hello world\n";
+	std::cout << PI << "\n";
+	return 0;
+}
+
+
